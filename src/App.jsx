ce8ae1df7419f2d843eaffe1ce8c2356c2b36e8d
Madhur Chauhan <madhurchauhan1100@gmail.com>
2025-01-07 12:23:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import About from "./Components/About/About"
+import Experience from "./Components/Experience/Experience"
+import Footer from "./Components/Footer/Footer"
+import Home from "./Components/Home/Home"
+import Navbar from "./Components/Navbar/Navbar"
+import Projects from "./Components/Projects/Projects"
+
+function App() {
+
+  return (
+    <div className="bg-[#171d32] w-full overflow-hidden h-auto">
+      <Navbar />
+      <Home />
+      <About />
+      <Experience />
+      <Projects />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import TextChange from "../TextChange";
+import avatarImg from "../../assets/Madhur1.jpg";
+
+const Home = () => {
+  return (
+    <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
+      <div className="md:w-2/4 md:pt-10 ">
+        <h1 className="text-xl md:text-6xl font-bold flex leading-normal tracking-tighter">
+          <TextChange />
+        </h1>
+        <p className="text-sm md:text-2xl tracking-tight ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+          doloremque?
+        </p>
+        <button className="mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+          Contact Me
+        </button>
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4">
+        <img src={avatarImg} alt="ProfilePhoto" className="rounded-full"/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
