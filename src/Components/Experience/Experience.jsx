f426a9086778e5b7526daa3cbc009bc8ff0def06
Madhur Chauhan <madhurchauhan1100@gmail.com>
2025-01-17 23:43:31 +0530

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,7 +3,7 @@ import { SiTypescript } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
 import { TbFileTypeSql } from "react-icons/tb";
 import CompanyLogo from "../../assets/CompanyLogo.jpg";
-const Experience = () => {
+const Experience = () => { 
   return (
     <div id="Experience" className="p-10 md:p-24">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Experience</h1>
